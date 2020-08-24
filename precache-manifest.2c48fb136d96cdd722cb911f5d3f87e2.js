0ca85b81d138b6a9a9d322b4a5559c932349bd8e
Pratik Bhadane <pratikbhadane100@gmail.com>
2020-08-24 10:00:21 +0530

    Updates

---
+++ precache-manifest.2c48fb136d96cdd722cb911f5d3f87e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4121a1e549676fbef2535de1c21ec715",
+    "url": "/facebook-react/index.html"
+  },
+  {
+    "revision": "715e596853af8767bb30",
+    "url": "/facebook-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6d76ac64657473068bd8",
+    "url": "/facebook-react/static/css/main.60efdadd.chunk.css"
+  },
+  {
+    "revision": "715e596853af8767bb30",
+    "url": "/facebook-react/static/js/2.5860d711.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/facebook-react/static/js/2.5860d711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d76ac64657473068bd8",
+    "url": "/facebook-react/static/js/main.930e8949.chunk.js"
+  },
+  {
+    "revision": "b9196d248df13a8c886c",
+    "url": "/facebook-react/static/js/runtime-main.7239589e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/facebook-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
